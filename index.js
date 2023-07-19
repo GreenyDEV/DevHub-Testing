41c356206d0bd9a60895836e55594825dc63c549
Darian <116446349+justDarian@users.noreply.github.com>
2023-07-18 21:30:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import url from 'url';
 import path from 'path';
 
 const app = express();
-const ultraviolet = new Easyviolet();
+const easyviolet = new Easyviolet();
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, 'static'), { extensions: ['html'] }));
 
 app.use((req, res, next) => {
-    if (!ultraviolet.requiresRoute(req)) res.status(404).sendFile(path.join(__dirname, 'static/404.html'));
+    if (!easyviolet.requiresRoute(req)) res.status(404).sendFile(path.join(__dirname, 'static/404.html'));
     else next();
 });
 
-const server = app.listen(process.env.PORT || 8080, () => console.log(`DevHub listening on port ${server.address().port}`));
+// STart Server
 
-ultraviolet.httpServer(server);
+const server = app.listen(80, () => console.log(`DevHub listening on port ${server.address().port}`));
+
+// Let EasyViolet attach to the server
+easyviolet.httpServer(server);
