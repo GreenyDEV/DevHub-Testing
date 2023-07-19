25a5223e5304cead3d5f0b3b4d153bd99edc9050
Darian <116446349+justDarian@users.noreply.github.com>
2023-07-18 21:20:23 -0400

    update frontend

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+navigator.serviceWorker.register('uv/sw.js', {
+  scope:"/uv/service/"
+}).then(e => console.log("Worker Registered Successfully")).catch(e => alert(e));
+
+window.loadstring = eval;
+fetch('/nav/nav.html')
+  .then(res => res.text())
+  .then(content => {
+    document.body.insertAdjacentHTML('afterbegin', content);
+    if (window.self !== window.top) {
+      window.parent.postMessage('loaded', location.origin);
+    }
+  }).catch(e => {
+    alert(e);
+  });
+
+fetch('js/assets.js').then(r => r.text()).then(r => eval(r));
+fetch('js/presets.js').then(r => r.text()).then(r => eval(r));
+fetch('js/load.js').then(r => r.text()).then(r => eval(r));
+fetch('js/search.js').then(r => r.text()).then(r => eval(r));
+//fetch('js/AT.js').then(r => r.text()).then(r => eval(r));
