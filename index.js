1e9c83f734acf8222b73794c9f35e37f6451f91d
Darian <116446349+justDarian@users.noreply.github.com>
2023-07-22 12:36:08 -0400

    whoops

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use('/uv/', express.static(uvPath));
 // 404 handler
 app.use((req, res) => res.status(404).sendFile(path.join(__dirname, './static/', '404.html')));
 
+// the following is checking if bare should handle the request or not
 server.on('request', (req, res) => {
     if (bare.shouldRoute(req)) {
         bare.routeRequest(req, res);
@@ -30,7 +31,7 @@ server.on('request', (req, res) => {
     }
 });
 
-server.on('upgrade', (req, res) => {
+server.on('upgrade', (req, socket, head) => {
     if (bare.shouldRoute(req)) {
         bare.routeUpgrade(req, socket, head);
     } else {
@@ -38,6 +39,7 @@ server.on('upgrade', (req, res) => {
     }
 });
 
+// start server
 server.listen(port, '0.0.0.0', () => {
     console.log("DevHub is listening on port "+server.address().port);
 });
