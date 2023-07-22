692de28898eb15a963d731f9f5b08c14c07a7d39
Darian <116446349+justDarian@users.noreply.github.com>
2023-07-22 12:09:53 -0400

    update backend

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview
+DevHub is a proxy hub, mostly meant to be a fast one stop shop for a high speed proxy, using.
+
+# How does it work?
+Using [Ultraviolet](https://github.com/titaniumnetwork-dev/Ultraviolet) and a private VPS, you can get top speeds and a game hub, right at your fingertips.
+
+# How to setup pls pls explain i want to host!!
+Download node.js, NPM, and git. Then download/clone the repository and run "npm install". After the packagaes are done downloading, either run "node index.js" or "npm start". You can modify the port in the index.js file.
+
+# Credits
+[Russell2259](https://github.com/Russell2259) and [Ultraviolet](https://github.com/titaniumnetwork-dev/Ultraviolet)
---
+++ index.js
@@ -0,0 +1,43 @@
+// Import Packages
+import http from 'node:http';
+import express from 'express';
+import * as url from 'url';
+import * as path from 'node:path';
+
+//Import proxy packages
+import createBareServer from '@tomphttp/bare-server-node';
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+
+// Variables
+const app = express();
+const server = http.createServer();
+const bare = createBareServer('/bare/');
+const port = process.argv[2] || 80;
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// display webpage
+app.use(express.static(path.join(__dirname, '/static'), { extensions: ['html'] }));
+// include ultraviolet scripts (autoupdate)
+app.use('/uv/', express.static(uvPath));
+// 404 handler
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, './static/', '404.html')));
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log("DevHub is listening on port "+server.address().port);
+});
